9d72185da0426b7913e7cf3369a0bf7c5555a4a2
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-09 09:05:24 +0600

    React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter as Router, Link } from '../node_modules/react-router-dom'
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Home</h1>
+
+
+        <Link to="/about">About</Link>
+        <Link to="/portfolio">Portfolio</Link>
+        <Link to="/contact">Contact</Link>
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/portfolio" element={<Portfolio/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
