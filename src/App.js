ea889dc7c26e483bf795aab6dde5a13a761ace04
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-09 09:35:05 +0600

    SCSS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import styles from './Mycss.module.css'
+import './App.scss'
 
 
 function App() {
-  {/* object css */ }
-  const style = {
-    color: "Green",
-    backgroundColor: 'lightgray',
-    width: "250px",
-    margin: "5px auto"
-  }
+
   return (
 
     <div className="App">
-      {/* inline css */}
-      <h1 style={{
-        color: "red", backgroundColor: 'lightblue', width: "250px", margin: "5px auto"
-      }}>Inline CSS</h1>
-      <h1 style={style}>Object Css</h1>
-      {/* External css */}
-      <h1 className="TextStyle">External Css</h1>
-      {/* Module css */}
-      <h1 className={styles.TextStyle}>Module Css</h1>
+      <h1 className="TextStyle">Jubaer Ahamed</h1>
     </div>
 
   );
