497e466735c0a4da6bd30e8f2beae1a72550d4f8
jubaerahamedbd <jubaerahamed@gmail.com>
2021-11-10 10:03:46 +0600

    Active NavLink

--- src/components/MyNavigation.js
+++ src/components/MyNavigation.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 class MyNavigation extends Component {
     render() {
         return (
             <div>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/pageone">Page One</Link></li>
-                    <li><Link to="/pagetwo">Page Two</Link></li>
-                    <li><Link to="/pagethree">Page Three</Link></li>
+                    <li><NavLink style={({ isActive }) => {
+                        return {
+                            color: isActive ? "red" : ""
+                        };
+                    }} to="/">Home</NavLink></li>
+                    <li><NavLink style={({ isActive }) => {
+                        return {
+                            color: isActive ? "red" : ""
+                        };
+                    }} to="/pageone">Page One</NavLink></li>
+                    <li><NavLink style={({ isActive }) => {
+                        return {
+                            color: isActive ? "red" : ""
+                        };
+                    }} to="/pagetwo">Page Two</NavLink></li>
+                    <li><NavLink style={({ isActive }) => {
+                        return {
+                            color: isActive ? "red" : ""
+                        };
+                    }} to="/pagethree">Page Three</NavLink></li>
                 </ul>
             </div>
         );
